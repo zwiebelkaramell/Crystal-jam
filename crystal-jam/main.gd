5cd999566da1ccaaa563915413d4b8d8c3dcb786
zwiebelkaramell <143242173+zwiebelkaramell@users.noreply.github.com>
2026-09-25 19:01:17 -0600

    Infinite roads

--- crystal-jam/main.gd
+++ crystal-jam/main.gd
@@ -1,11 +1,35 @@
 extends Node3D
-
+var asphalt_scene = preload("res://asphalt.tscn")
 
 
 
 func _process(delta: float) -> void:
+	
+	roadmove()
+	
 	pass
 
 
 func _input(event: InputEvent) -> void:
 	pass
+
+
+# road movement subroutine
+func roadmove():
+	var road = $Road
+	var asphalt_instances = road.get_children()
+	var movespeed = 2
+	
+	road.position.x -= movespeed
+	
+	for n in asphalt_instances:
+		if n.global_position.x <= -200:
+			n.queue_free()
+			for x in asphalt_instances:
+				print(x)
+			print("break")
+			var asphalt = asphalt_scene.instantiate()
+			asphalt.position.x = (n.position.x+400)
+			road.add_child(asphalt)
+			
+		
